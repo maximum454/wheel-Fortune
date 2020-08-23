f154a372b6be5b2547a27bd4079f333329b44354
maxim <maximum454@gmail.com>
2020-08-23 19:51:44 +0600

    fix

--- src/js/whfortune.js
+++ src/js/whfortune.js
@@ -15,13 +15,8 @@ $(document).ready(function () {
             return false
         }
         $(this).slideUp();
+
         $('.fortune__itog').slideUp();
-
-        var $pointer = $('.fortune__point');
-        var pointerX = $pointer.offset().left + $pointer.width() * 0.5;
-        var pointerY = $pointer.offset().top + ($pointer.height() - 40) * 0.5;
-        $pointer.hide();
-
         var newDegree = degree * clicks;
         var extraDegree = Math.floor(Math.random() * (360 - 1 + 1)) + 1;
         totalDegree = newDegree + extraDegree;
@@ -31,13 +26,17 @@ $(document).ready(function () {
                 'transform': 'rotate(' + totalDegree + 'deg)'
             });
         });
-
+        var $pointer = $('.fortune__point');
+        var pointerX = $pointer.offset().left + $pointer.width() * 0.5;
+        var pointerY = $pointer.offset().top + $pointer.height() * 0.5;
+        $pointer.hide();
         setTimeout(function () {
             var prize = document.elementFromPoint(pointerX, pointerY);
             var txt = $(prize).data('prize');
+            console.log(prize);
             $('.fortune__prize').html(txt);
             $('.fortune__itog').slideDown();
-        }, 5500);
+        }, 6000);
     })
 })
 
